27856952020177a4e2d488ec7d9254d59a92c2c5
Iris Sanders <irissanders@live.nl>
2024-07-12 00:10:47 +0200

    added error

--- src/main.c
+++ src/main.c
@@ -6,22 +6,32 @@
 /*   By: irsander <irsander@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/17 13:30:21 by irsander          #+#    #+#             */
-/*   Updated: 2024/07/11 23:49:10 by irsander         ###   ########.fr       */
+/*   Updated: 2024/07/12 00:10:30 by irsander         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
+static void	ft_error(char *msg)
+{
+	ft_putstr_fd("Error: ", STDERR_FILENO);
+	ft_putstr_fd(msg, STDERR_FILENO);
+	ft_putstr_fd("\n", STDERR_FILENO);
+	exit(EXIT_FAILURE);
+}
+
 int main(int argc, char **argv)
 {
-    char    *command_line_input;
+    char    *input;
 
     (void)argc;
     (void)argv;
     while (1)
     {
-        command_line_input = readline(PROMPT);
-        ft_printf("%s", command_line_input);
+        input = readline(PROMPT);
+        if (input == NULL)
+            ft_error("Failed to read input");
+        free(input);
     }
     return(0);
 }
